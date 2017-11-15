7bcfc988e344051cd310fb1bddec0166fd60a046
Qoli Wong <llqoli@gmail.com>
2017-11-15 23:32:52 +0800

    修正 3 行顯示 ... 的問題。

--- Extension/TodayViewController.swift
+++ Extension/TodayViewController.swift
@@ -46,9 +46,42 @@ class TodayViewController: UIViewController, NCWidgetProviding {
                 s = s.trimmingCharacters(in: .whitespacesAndNewlines)
                 
                 self.todayWidget.text = s
+            
+                self.todayWidget.typesetting(lineSpacing: 1, lineHeightMultiple: 1, characterSpacing: 1.2)
                 
             }
         }
     }
     
 }
+
+/*
+ setLineSpacing 設定行高 / lineHeightMultiple / 字距
+ */
+extension UILabel {
+    
+    func typesetting(lineSpacing: CGFloat = 0.0, lineHeightMultiple: CGFloat = 0.0, characterSpacing: CGFloat = 0.0) {
+        
+        guard let labelText = self.text else { return }
+        
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.lineHeightMultiple = lineHeightMultiple
+        
+        let attributedString: NSMutableAttributedString
+        if let labelattributedText = self.attributedText {
+            attributedString = NSMutableAttributedString(attributedString: labelattributedText)
+        } else {
+            attributedString = NSMutableAttributedString(string: labelText)
+        }
+        
+        // 處理字距
+        attributedString.addAttribute(NSAttributedStringKey.kern, value: characterSpacing, range: NSRange(location: 0, length: attributedString.length - 1))
+        
+        // 處理行高
+        attributedString.addAttribute(NSAttributedStringKey.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
+        
+        self.attributedText = attributedString
+    }
+}
+
